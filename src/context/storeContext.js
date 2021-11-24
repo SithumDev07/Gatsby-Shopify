2c38fcd7f2472e0cf99b24014d104dda8626b0cd
Sithum Basnayaka <sithum.dashantha@gmail.com>
2021-11-24 07:02:49 +0530

    storeContext

---
+++ src/context/storeContext.js
@@ -0,0 +1,177 @@
+import React from 'react'
+import fetch from 'isomorphic-fetch'
+import Client from 'shopify-buy'
+import { createContext } from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { useContext } from 'react';
+
+const client = Client.buildClient({
+    domain: process.env.GATSBY_SHOPIFY_STORE_URL,
+    storefrontAccessToken: process.env.GATSBY_STOREFRONT_ACCESS_TOKEN
+}, fetch);
+
+const defaultValues = {
+    cart: [],
+    loading: false,
+    addVariantToCart: () => {},
+    removeLineItem: () => {},
+    client,
+    checkout: {
+        id: "",
+        lineItems: [],
+        webUrl: ""
+    }
+}
+
+const StoreContext = createContext(defaultValues);
+
+const isBrowser = typeof window!= 'undefined';
+const localStorageKey = `shopify_checkout_id`;
+
+export const StoreProvider = ({ children }) => {
+    const [cart, setCart] = useState(defaultValues.cart);
+    const [checkout, setCheckout] = useState(defaultValues.checkout);
+    const [loading, setLoading] = useState(defaultValues.loading);
+
+    const setCheckoutItem = (checkout) => {
+        if(isBrowser) {
+            localStorage.setItem(localStorageKey, checkout.id);
+        }
+
+        setCheckout(checkout);
+    }
+
+    useEffect(() => {
+        const initializeCheckout = async () => {
+            const existingCheckoutID = isBrowser
+            ? localStorage.getItem(localStorageKey)
+            : null;
+
+            if(existingCheckoutID && existingCheckoutID !== `null`) {
+                try{
+                    const existingCheckout = await client.checkout.fetch(existingCheckoutID);
+
+                    if(!existingCheckout.completedAt) {
+                        setCheckoutItem(existingCheckout)
+                        return
+                    }
+                } catch (e) {
+                    localStorage.setItem(localStorageKey, null)
+                }
+            }
+
+
+            const newCheckout = await client.checkout.create()
+            setCheckoutItem(newCheckout)
+        }
+
+        initializeCheckout()
+    }, [])
+
+    const addVariantToCart = async (product, quantity) => {
+        setLoading(true)
+
+        if(checkout.id === "") {
+            console.error("No Checkout ID Assigned");
+            return
+        }
+
+        const checkoutID = checkout.id;
+        const variantID = product.variants[0]?.shopifyId;
+        const parsedQuantity = parseInt(quantity, 10);
+
+        const lineItemsToUpdate = [
+            {
+                variantID,
+                quantity: parsedQuantity
+            }
+        ]
+
+        try {
+            const res = await client.checkout.addLineItems(checkoutID, lineItemsToUpdate);
+            setCheckout(res);
+
+            let updatedCart = [];
+            if(cart.length > 0){
+                const itemsInCart = cart.find((item) => item.product.variants[0]?.shopifyId === variantID);
+
+                if(itemsInCart) {
+                    const newProduct = {
+                        product: {...itemsInCart.product},
+                        quantity: (itemsInCart.quantity * parsedQuantity)
+                    }
+
+                    const otherItems = cart.filter((item) => item.product.variants[0].shopifyId !== variantID)
+                    updatedCart = [...otherItems, newProduct]
+                } else {
+                    updatedCart = cart.concat([{product, quantity: parsedQuantity}])
+                }
+            } else {
+                updatedCart = [{product, quantity: parsedQuantity}]
+            }
+
+            setCart(updatedCart)
+
+            setLoading(false)
+            alert('item added to cart')
+        } catch (error) {
+            setLoading(false)
+            console.error(`Error in addVariantToCart: ${error}`);
+        }
+    }
+
+    const removeLineItem = async (variantId) => {
+        setLoading(true)
+        try {
+            let lineItemID = ""
+            checkout.lineItems?.forEach((item) => {
+                if(item.variableValues.lineItems[0].variantId === variantId){
+                    lineItemID = item.id
+                }
+            })
+
+            if(!lineItemID) {
+                console.log('Product not in cart');
+                return
+            }
+
+            const res = await client.checkout.removeLineItems(checkout.id, [lineItemID]);
+            setCheckout(res)
+
+            const updatedCart = cart.filter((item) => item.product.variants[0]?.shopifyId !== variantId);
+            setCart(updatedCart)
+
+            setLoading(false)
+        } catch (error) {
+            setLoading(false)
+            console.error("Error in removeLineItems");
+        }
+    }
+
+    return (
+        <StoreContext.Provider value={{ 
+            ...defaultValues,
+            addVariantToCart,
+            removeLineItem,
+            cart,
+            checkout, 
+            loading
+         }}>
+            {children}
+        </StoreContext.Provider>
+    )
+}
+
+const useStore = () => {
+    const context = useContext(StoreContext)
+
+    if(context === undefined) {
+        throw new Error('userStore must be used in StoreContext')
+    }
+
+    return context
+}
+
+
+export default useStore
